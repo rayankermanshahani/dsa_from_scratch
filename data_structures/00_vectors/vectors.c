ac7f533c797cc8baaeb7a347a5ae413c7f1ec193
rayankermanshahani <rayan.k@gmx.com>
2024-07-31 08:33:39 -0400

    completed vectors

--- data_structures/00_vectors/vectors.c
+++ data_structures/00_vectors/vectors.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 typedef struct {
-    int* elements;
+    int* data;
     size_t size;
     size_t capacity;
 } vector;
@@ -11,13 +11,12 @@ typedef struct {
 vector* create_vector(size_t initial_capacity) {
     /* allocate memory for the vector object itself */
     vector* vec = (vector*)malloc(sizeof(vector)); 
-    if (vec == NULL) {
+    if (vec == NULL)
         return NULL;
-    }
 
-    /* allocate memory for the vector's elements */
-    vec->elements = (int*)malloc(initial_capacity * sizeof(int));
-    if (vec->elements == NULL) {
+    /* allocate memory for the vector's data */
+    vec->data = (int*)malloc(initial_capacity * sizeof(int));
+    if (vec->data == NULL) {
         free(vec);
         return NULL;
     }
@@ -29,61 +28,96 @@ vector* create_vector(size_t initial_capacity) {
 
 void free_vector(vector* vec) {
     if (vec) {
-        free(vec->elements);
+        free(vec->data);
         free(vec);
     }
 }
 
 int resize(vector* vec) {
     size_t new_capacity = vec->capacity * 2; /* double the vector's capacity */
-    int* new_elements = (int*)realloc(vec->elements, new_capacity * sizeof(int));
-    if (new_elements == NULL) {
+    int* new_data = (int*)realloc(vec->data, new_capacity * sizeof(int));
+    if (new_data == NULL)
         return 0; /* resize failed */
-    }
 
-    vec->elements = new_elements;
+    vec->data = new_data;
     vec->capacity = new_capacity;
     return 1; /* resize successful */
 }
 
-int insert(vector* vec, int elem) {
-    if (vec->size == vec->capacity) {
-        if (!resize(vec)) {
-            return 0; /* insert failed */ 
-        }
+int append(vector* vec, int elem) {
+    if (vec->size == vec->capacity)
+        if (!resize(vec)) 
+            return 0; /* append failed */ 
+    vec->data[vec->size++] = elem;
+    return 1; /* append successful */
+}
+
+int delete(vector* vec, size_t index) {
+    if (index < 0 || index >= vec->size) {
+        printf("delete() error: invalid index\n");
+        return 0; /* invalid index */
     }
-    vec->elements[vec->size++] = elem;
-    return 1; /* insert successful */
+    for (size_t i = index; i < vec->size-1; i++) {
+        vec->data[i] = vec->data[i+1];
+    }
+    vec->size--;
+    if (vec->size > 0 && vec->size == vec->capacity)
+        resize(vec); 
+    return 1;
+}
+
+int replace(vector* vec, size_t index, int elem) {
+    if (index < 0 || index >= vec->size) {
+        printf("replace() error: invalid index\n");
+        return 0; /* invalid index */
+    }
+    vec->data[index] = elem;
+    return 1; /* replace successful */
 }
 
 int get(vector* vec, size_t index, int* elem) {
-    if (index >= vec->size) {
+    if (index < 0 || index >= vec->size) {
+        printf("get() error: invalid index\n");
         return 0; /* invalid index */
     }
-    *elem = vec->elements[index];
+    *elem = vec->data[index];
     return 1; /* get successful */
 }
 
 void print_vector(vector* vec) {
     printf("vector (size: %zu, capacity: %zu): [ ", vec->size, vec->capacity);
     for (size_t i = 0; i < vec->size; i++) {
-        printf("%d, ", vec->elements[i]);
+        printf("%d, ", vec->data[i]);
     }
     printf("]\n");
 }
 
 void test_vector() {
-    vector* vec = create_vector(2);
-    insert(vec, 1);
-    insert(vec, 3); 
-    insert(vec, 3); /* this insert() should trigger a call to resize() */
-    insert(vec, 7);
+    vector* vec = create_vector(2); /* vec: [ , ,] */
+    append(vec, 1); /* vec: [1, ,] */
+    append(vec, 3); /* vec: [1, 3,] */
+    append(vec, 3); /* vec: [1, 3, 3, ,]; triggers resize() */
+    append(vec, 6); /* vec: [1, 3, 3, 6,] */
+    replace(vec, 3, 4); /* vec: [1, 3, 3, 4,] */
+    append(vec, 9); /* vec: [1, 3, 3, 4, 9, , , ,]; trigger resize() */
+    append(vec, 7); /* vec: [1, 3, 3, 4, 9, 7, , ,] */
+    delete(vec, 3); /* vec: [1, 3, 3, 9, 7, , , ,] */
+    delete(vec, 3); /* vec: [1, 3, 3, 7, , , , ,] */
 
     print_vector(vec);
 
     int elem;
+    if (get(vec, 0, &elem)) {
+        printf("element at index 0: %d\n", elem);
+    }
+    if (get(vec, 1, &elem)) {
+        printf("element at index 1: %d\n", elem);
+    }
     if (get(vec, 2, &elem)) {
         printf("element at index 2: %d\n", elem);
+    }
+    if (get(vec, 3, &elem)) {
+        printf("element at index 3: %d\n", elem);
     }
 
     free_vector(vec);
